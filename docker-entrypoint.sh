78149a0d491e828b1ad5ca4ba8f963de40151162
Jamie Starke <git@jamiestarke.com>
2022-10-09 19:28:34 -0700

    Updated Huggingface api token to be consistent with upstream peacasso

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ -z "${HUGGINGFACE_ACCESS_TOKEN}" ]; then
-  echo "The HUGGINGFACE_ACCESS_TOKEN was not set. This can be created"
+if [ -z "${HF_API_TOKEN}" ]; then
+  echo "The HF_API_TOKEN was not set. This can be created"
   echo "by logging in to https://huggingface.co/CompVis/stable-diffusion-v-1-4-original"
   echo "then going to https://huggingface.co/settings/tokens to retrieve an access token"
   exit 1
@@ -11,7 +11,7 @@ MODEL_FILENAME=$(echo ${STABLE_DIFFUSION_MODEL_URL} | sed -r 's#.*/##g')
 if [ ! -f "${MODEL_FILENAME}" ]; then
   echo "Downloading the Stable Diffusion Model: ${STABLE_DIFFUSION_MODEL_URL}"
   curl \
-  -H "Authorization: Bearer ${HUGGINGFACE_ACCESS_TOKEN}" \
+  -H "Authorization: Bearer ${HF_API_TOKEN}" \
   -L -O \
   ${STABLE_DIFFUSION_MODEL_URL}
 fi
